b99549d80b95f80df7b328f4afaf5934774ea525
Bram Decuypere <bram.decuypere@gmail.com>
2018-06-12 18:11:10 +0200

    binBooking - reorganize booking component + tests

--- src/booking.js
+++ src/booking.js
@@ -1,14 +1,7 @@
 (function (angular) {
-    angular.module('bin.booking', ['binarta-applicationjs-angular1'])
+    angular.module('bin.booking', ['binarta-applicationjs-angular1', 'momentx'])
         .service('binBooking', ['binarta', BinBookingService])
         .component('binBooking', new BookingComponent());
-
-    var defaultDateFormat = 'YYYY-MM-DD';
-    var defaultLocaleParamName = 'Language';
-    var defaultArrivalParamName = 'Arrival';
-    var defaultDepartureParamName = 'Departure';
-    var defaultDiscountParamName = 'DiscountCode';
-    var defaultUrl = 'https://reservations.cubilis.eu/HOTEL-ID/Rooms/Select'; //Existing hotel-id = 6331
 
     function BinBookingService(binarta) {
         var bookingConfigCode = 'booking.config';
@@ -45,34 +38,84 @@
 
     function BookingComponent() {
         this.templateUrl = 'bin-booking.html';
+        this.bindings = {
+            dateFormat: '@',
+
+            localeParamName: '@',
+            arrivalParamName: '@',
+            departureParamName: '@',
+            discountParamName: '@',
+
+            baseUrl: '@',
+            hotelId: '@',
+            urlSuffix: '@',
+            completeUrlWithoutParams: '@',
+
+            discountCode: '@',
+        };
         this.controller = ['$scope', '$window', 'binBooking', 'moment', 'binarta', binComponentController(function ($scope, $window, binBooking, moment, binarta) {
             var $ctrl = this;
-            $ctrl.minArrivalDate = moment();
-            $ctrl.minDepartureDate = moment().add(1, 'days');
-            $scope.arrivalDate = moment().add(1, 'days');
-            $scope.departureDate = moment().add(3, 'days');
 
-            $ctrl.submit = function () {
-                binBooking.findConfig(function (config) {
-                    var params = config.params || {};
-                    var dateFormat = params.dateFormat || defaultDateFormat;
-                    var url = config.url;
-                    var localeParam = params.locale || defaultLocaleParamName;
-                    var arrivalParam = params.arrival || defaultArrivalParamName;
-                    var departureParam = params.departure || defaultDepartureParamName;
-                    var discountParam = params.discount || defaultDiscountParamName;
 
-                    var arrivalDate = moment($scope.arrivalDate).format(dateFormat);
-                    var departureDate = moment($scope.departureDate).format(dateFormat);
+            $ctrl.$onInit = function () {
+                $ctrl.minArrivalDate = moment();
+                $ctrl.minDepartureDate = moment().add(1, 'days');
+                $scope.arrivalDate = moment().add(1, 'days');
+                $scope.departureDate = moment().add(3, 'days');
 
-                    url += getQueryStringSeperator(url) + localeParam + '=' + binarta.application.localeForPresentation();
-                    url += getQueryStringSeperator(url) + arrivalParam + '=' + arrivalDate;
-                    url += getQueryStringSeperator(url) + departureParam + '=' + departureDate;
-                    if ($scope.discountCode) url += getQueryStringSeperator(url) + discountParam + '=' + $scope.discountCode;
-    
-                    $window.open(url);
+
+                if ($ctrl.completeUrlWithoutParams && ($ctrl.baseUrl || $ctrl.hotelId || $ctrl.urlSuffix)) {
+                    throw new Error('You should use a completeUrlWithoutParams or the separete segments baseUrl, hotelId, urlSuffix');
+                }
+
+                binBooking.findConfig(function (_config_) {
+                    $ctrl.config = _config_;
                 });
             };
+
+            $ctrl.submit = function () {
+                var params = $ctrl.config.params || {};
+
+                if ($ctrl.completeUrlWithoutParams)
+                    $ctrl.url = $ctrl.completeUrlWithoutParams;
+                else if ($ctrl.baseUrl && $ctrl.hotelId && $ctrl.urlSuffix)
+                    $ctrl.url = $ctrl.composeBookingUrl($ctrl.baseUrl, $ctrl.hotelId, $ctrl.urlSuffix);
+                else if ($ctrl.config.url)
+                    $ctrl.url = $ctrl.config.url;
+                else throw new Error('Should have a URL to navigate too');
+
+
+                $ctrl.dateFormat = $ctrl.dateFormat || params.dateFormat || 'YYYY-MM-DD';
+                $ctrl.localeParamName = $ctrl.localeParamName || params.locale || 'Language';
+                $ctrl.arrivalParamName = $ctrl.arrivalParamName || params.arrival || 'Arrival';
+                $ctrl.departureParamName = $ctrl.departureParamName || params.departure || 'Departure';
+                $ctrl.discountParamName = $ctrl.discountParamName || params.discount || 'DiscountCode';
+
+                var arrivalDate = moment($scope.arrivalDate).format($ctrl.dateFormat);
+                var departureDate = moment($scope.departureDate).format($ctrl.dateFormat);
+
+                $ctrl.url += getQueryStringSeperator($ctrl.url) + $ctrl.localeParamName + '=' + binarta.application.localeForPresentation();
+                $ctrl.url += getQueryStringSeperator($ctrl.url) + $ctrl.arrivalParamName + '=' + arrivalDate;
+                $ctrl.url += getQueryStringSeperator($ctrl.url) + $ctrl.departureParamName + '=' + departureDate;
+
+                if ($ctrl.discountCode) {
+                    $ctrl.url += getQueryStringSeperator($ctrl.url) + $ctrl.discountParamName + '=' + $ctrl.discountCode;
+                }
+
+                $window.open($ctrl.url);
+            };
+
+            $ctrl.composeBookingUrl = function (baseUrl, hotelId, urlSuffix) {
+                if (baseUrl[baseUrl.length - 1] !== '/') {
+                    throw new Error('Base URL should end with a "/"');
+                }
+
+                if (urlSuffix[0] === '/') {
+                    throw new Error('urlSuffix can\'t start with a "/"');
+                }
+
+                return baseUrl + hotelId + '/' + urlSuffix;
+            }
 
             function getQueryStringSeperator(url) {
                 return url.indexOf('?') === -1 ? '?' : '&';
--- test/booking.spec.js
+++ test/booking.spec.js
@@ -35,8 +35,8 @@ describe('binBooking service', function () {
             sut.updateConfig({
                 url: 'test'
             }, {
-                error: errorSpy
-            });
+                    error: errorSpy
+                });
 
             expect(errorSpy).toHaveBeenCalledWith('url.invalid');
         });
@@ -65,8 +65,8 @@ describe('binBooking service', function () {
             sut.updateConfig({
                 url: validUrl
             }, {
-                success: successSpy
-            });
+                    success: successSpy
+                });
 
             expect(successSpy).toHaveBeenCalled();
         });
@@ -91,4 +91,296 @@ describe('binBooking service', function () {
             });
         });
     });
+});
+
+describe('binBooking component', function () {
+    var $ctrl;
+    var moment, $componentController;
+    var scope;
+    var windowMock;
+    var bookingBaseUrl = 'https://reservations.cubilis.eu/6331/Rooms/Select';
+    var openUrlSpy;
+    var bookingConfigCode;
+    var binBooking;
+    var genericMomentDate;
+
+
+    beforeEach(inject(function (_$componentController_, _moment_, $rootScope, _binBooking_) {
+        moment = _moment_;
+        $componentController = _$componentController_;
+        binBooking = _binBooking_;
+        genericMomentDate = '2019-08-05';
+
+        binBooking.updateConfig({
+            url: bookingBaseUrl.slice(),
+        });
+
+        openUrlSpy = jasmine.createSpy('open');
+
+        bookingConfig = {
+            params: {
+                dateFormat: 'YYYY',
+                locale: 'Language',
+                departure: 'Departure',
+                arrival: 'Arrival',
+                discount: 'Discount',
+            },
+            url: bookingBaseUrl,
+        };
+
+        windowMock = {
+            open: openUrlSpy,
+        };
+
+        binartaMock = {
+            application: {
+                localeForPresentation: function () {
+                    return 'nl-NL';
+                },
+            },
+        };
+
+        scope = $rootScope.$new();
+
+        const momentFunc = function () {
+            return moment(genericMomentDate);
+        };
+
+        momentMock = momentFunc;
+
+        $ctrl = $componentController('binBooking', {
+            $scope: scope,
+            $window: windowMock,
+            binarta: binartaMock,
+            moment: momentMock,
+        });
+    }));
+
+    describe('Initializing component -', function () {
+
+        beforeEach(function () {
+            $ctrl.$onInit();
+        });
+
+        it('Should have a minimumArrivalDate initialized', function () {
+            expect($ctrl.minArrivalDate instanceof moment).toBeTruthy();
+        });
+
+        it('Should have a minDepartureDate initialized', function () {
+            expect($ctrl.minDepartureDate instanceof moment).toBeTruthy();
+        });
+
+        it('Should have a arrivalDate initialized', function () {
+            expect(scope.arrivalDate instanceof moment).toBeTruthy();
+        });
+
+        it('Should have a departureDate initialized', function () {
+            expect(scope.departureDate instanceof moment).toBeTruthy();
+        });
+
+        it('Should have a config initialized', function () {
+            expect($ctrl.config.url).toBe(bookingBaseUrl);
+        });
+
+        describe('On error -', function () {
+
+            it('Should throw an error when both a completeBaseUrl and a baseUrl are passed', function () {
+                $ctrl.completeUrlWithoutParams = bookingBaseUrl.slice();
+                $ctrl.baseUrl = 'https://reservations.cubilis.eu/';
+                try {
+                    $ctrl.$onInit();
+                } catch (err) {
+                    expect(err.message).toBe('You should use a completeUrlWithoutParams or the separete segments baseUrl, hotelId, urlSuffix');
+                }
+            });
+
+
+            it('Should throw an error when both a completeBaseUrl and a hotelId are passed', function () {
+                $ctrl.completeUrlWithoutParams = bookingBaseUrl.slice();
+                $ctrl.hotelId = '6631';
+                expect(function () {
+                    $ctrl.$onInit();
+                }).toThrowError();
+            });
+
+
+            it('Should throw an error when both a completeBaseUrl and a urlSuffix are passed', function () {
+                $ctrl.completeUrlWithoutParams = bookingBaseUrl.slice();
+                $ctrl.urlSuffix = 'Rooms/Select';
+                expect(function () {
+                    $ctrl.$onInit();
+                }).toThrowError();
+            });
+        });
+    });
+
+    describe('Submit -', function () {
+
+        function getParamsArr(url) {
+            var params = url.split('?')[1];
+            var obj = {};
+            params.split('&').map(function (el) {
+                var split = el.split('=');
+                obj[split[0]] = split[1];
+            });
+            return obj;
+        }
+
+        it('Should open a new window', function () {
+            $ctrl.$onInit();
+            $ctrl.submit();
+            expect(windowMock.open).toHaveBeenCalledWith('https://reservations.cubilis.eu/6331/Rooms/Select?Language=nl-NL&Arrival=2019-08-05&Departure=2019-08-05');
+        });
+
+        it('Should have the correct base URL', function () {
+            $ctrl.config.url = 'http://config.be/6634/Rooms/Select'
+            $ctrl.submit();
+            expect($ctrl.url.split('?')[0]).toBe($ctrl.config.url);
+
+            $ctrl.baseUrl = 'http://completeUrlWithoutParams.be/';
+            $ctrl.hotelId = '6344';
+            $ctrl.urlSuffix = 'Rooms/Select';
+            $ctrl.submit();
+            expect($ctrl.url.split('?')[0]).toBe($ctrl.composeBookingUrl($ctrl.baseUrl, $ctrl.hotelId, $ctrl.urlSuffix));
+
+            $ctrl.completeUrlWithoutParams = 'http://completeUrlWithoutParams.be';
+            $ctrl.submit();
+            expect($ctrl.url.split('?')[0]).toBe($ctrl.completeUrlWithoutParams);
+        });
+
+        describe('Params in url -', function () {
+            beforeEach(function () {
+                $ctrl.config.url = bookingBaseUrl;
+            });
+
+            it('Should add the localeparam', function () {
+                $ctrl.submit();
+                expect(getParamsArr($ctrl.url)[$ctrl.localeParamName]).toBe(binartaMock.application.localeForPresentation());
+            });
+
+            it('Should add the arrivalParam', function () {
+                $ctrl.submit();
+                expect(getParamsArr($ctrl.url)[$ctrl.arrivalParamName]).toBe(genericMomentDate);
+            });
+
+            it('Should add the departureParam', function () {
+                $ctrl.submit();
+                expect(getParamsArr($ctrl.url)[$ctrl.departureParamName]).toBe(genericMomentDate);
+            });
+
+            it('Should add discountCode if available', function () {
+                $ctrl.submit();
+                expect(getParamsArr($ctrl.url)[$ctrl.discountParamName]).toBe(undefined);
+
+                $ctrl.discountCode = 'ALL-FREE';
+                $ctrl.submit();
+                expect(getParamsArr($ctrl.url)[$ctrl.discountParamName]).toBe($ctrl.discountCode);
+            });
+        });
+
+        describe('Config params available, but no bindings -', function () {
+
+            beforeEach(function () {
+                $ctrl.config = bookingConfig;
+                $ctrl.submit();
+            });
+
+            it('Should use the config param for dateFormat', function () {
+                expect($ctrl.dateFormat).toBe(bookingConfig.params.dateFormat);
+            });
+
+            it('Should use the config param for locale', function () {
+                expect($ctrl.localeParamName).toBe(bookingConfig.params.locale);
+            });
+
+            it('Should use the config param for arrival', function () {
+                expect($ctrl.arrivalParamName).toBe(bookingConfig.params.arrival);
+            });
+
+            it('Should use the config param for departure', function () {
+                expect($ctrl.departureParamName).toBe(bookingConfig.params.departure);
+            });
+
+            it('Should use the config param for discount', function () {
+                expect($ctrl.discountParamName).toBe(bookingConfig.params.discount);
+            });
+        });
+
+        describe('Default values available, no bindings and no config params -', function () {
+
+            beforeEach(function () {
+                $ctrl.$onInit();
+                $ctrl.submit();
+            });
+
+            it('Should have a default for dateFormat', function () {
+                expect($ctrl.dateFormat).toBe('YYYY-MM-DD');
+            });
+
+            it('Should have a default paramName for locale', function () {
+                expect($ctrl.localeParamName).toBe('Language');
+            });
+
+            it('Should have a default paramName for arrival', function () {
+                expect($ctrl.arrivalParamName).toBe('Arrival');
+            });
+
+            it('Should have a default paramName for departure', function () {
+                expect($ctrl.departureParamName).toBe(bookingConfig.params.departure);
+            });
+
+            it('Should have a default paramName for discount', function () {
+                expect($ctrl.discountParamName).toBe('DiscountCode');
+            });
+        });
+
+        describe('On Errors -', function () {
+
+            beforeEach(function () {
+                $ctrl.$onInit();
+            });
+
+            it('Should throw an error when no url is provided', function () {
+                $ctrl.config.url = undefined;
+                expect($ctrl.submit).toThrowError('Should have a URL to navigate too');
+            });
+
+
+        });
+    });
+
+    describe('ComposeUrl -', function () {
+        var baseUrl;
+        var hotelId;
+        var urlSuffix;
+        var composedUrl;
+
+
+        beforeEach(function () {
+            baseUrl = 'https://reservations.cubilis.eu/';
+            hotelId = '6331';
+            urlSuffix = 'Rooms/Select';
+            composedUrl = $ctrl.composeBookingUrl(baseUrl, hotelId, urlSuffix);
+        });
+
+        it('Should return a composed URL', function () {
+            expect(composedUrl).toEqual(bookingBaseUrl);
+        });
+
+        describe('on Error -', function () {
+
+            it('Should throw an error if baseUrl doesn\'t end with "/"', function () {
+                expect(function () {
+                    $ctrl.composeBookingUrl('http://url.be', hotelId, urlSuffix);
+                }).toThrowError();
+            });
+
+            it('Should throw an error if urlSuffix starts with "/"', function () {
+                expect(function () {
+                    $ctrl.composeBookingUrl(baseUrl, hotelId, '/' + urlSuffix);
+                }).toThrowError('urlSuffix can\'t start with a "/"');
+            });
+        });
+    });
+
 });
